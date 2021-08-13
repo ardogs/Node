b5128219c0c32905f5179eb9bd0bea0bb586b098
Jesus Martín <jesus.martinsamano@gmail.com>
2021-08-13 18:01:27 -0500

    ----

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,17 +1,22 @@
-
-const { crearArchivo } = require('./helpers/multiplicar')
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv =  require('yargs').argv;
 
 console.clear();
 
-console.log(process.argv)
-const [, , arg3 = 'base=5'] =  process.argv;
-const [, base = 5] = arg3.split('=');
+console.log(process.argv);
+console.log(argv);
+
+console.log('base: yargs', argv.base);
+
+// console.log(process.argv)
+// const [, , arg3 = 'base=5'] =  process.argv;
+// const [, base = 5] = arg3.split('=');
 // console.log(base);
 
 
 
 // const base = 10;
 
-crearArchivo( base )
-    .then( archivo => console.log(archivo, 'creado') )
-    .catch( err => console.log(err));
+// crearArchivo( base )
+//     .then( archivo => console.log(archivo, 'creado') )
+//     .catch( err => console.log(err));
